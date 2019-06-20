a479f629959850f071c94fe2bed6e0ead64b3ede
nurja1218 <nurja1218@gmail.com>
2019-06-20 21:53:35 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const studentInfo = {
+  name: "junyoung",
+  age: 30,
+  gender: "Male",
+  liveIn: "gyeongGi",
+  favMovies: ["7 days", "Old boy", "Iron Man", "Braveheart"],
+  favFood: [
+    {
+      name: "Kimchi",
+      fatty: true
+    },
+    {
+      name: "Big Mac",
+      fatty: true
+    }
+  ]
+};
+
+console.log(studentInfo.favFood[0].fatty);
+  
